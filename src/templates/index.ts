af4cff7848b980430f35fc6d469d68ab742d6d39
Dhika Rizky <deka_r@outlook.com>
2021-01-16 05:22:05 +0700

    Added amukan army copypasta

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -2,10 +2,12 @@ import Simp from './simp'
 import Kawan from './kawan'
 import Wangy from './wangy'
 import DateWaifu from './dateWaifu'
+import AmukanArmy from './amukanArmy'
 
 export default [
   Simp,
   Kawan,
   Wangy,
-  DateWaifu
+  DateWaifu,
+  AmukanArmy
 ]
