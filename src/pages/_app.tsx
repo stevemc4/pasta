c85c8b604cbdb611d0d11b409649780735c2f9b9
Dhika Rizky <deka_r@outlook.com>
2021-01-15 23:19:30 +0700

    Initialized Chakra UI

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
+const App = ({ Component, pageProps }: AppProps): React.ReactElement => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default App
