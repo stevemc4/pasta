5047b245629791e721bbbd5533c345e7aa97e7cd
Dhika Rizky <deka_r@outlook.com>
2021-01-16 05:24:57 +0700

    Added tim sepakbola copypasta

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -3,11 +3,13 @@ import Kawan from './kawan'
 import Wangy from './wangy'
 import DateWaifu from './dateWaifu'
 import AmukanArmy from './amukanArmy'
+import TimSepakbola from './timSepakbola'
 
 export default [
   Simp,
   Kawan,
   Wangy,
   DateWaifu,
-  AmukanArmy
+  AmukanArmy,
+  TimSepakbola
 ]
