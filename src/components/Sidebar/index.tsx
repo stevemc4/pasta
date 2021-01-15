801a1747266474ab2480ccb8111f1c95f144685f
Dhika Rizky <deka_r@outlook.com>
2021-01-16 03:59:06 +0700

    Completed basic functionality

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Box, Heading, List, ListItem, Text, Button } from '@chakra-ui/react'
+
+import Template from '../../types/Template'
+
+import Templates from '../../templates'
+
+interface Props {
+  onSelect: (template: Template) => void
+}
+
+const Sidebar = ({ onSelect }: Props): React.ReactElement => {
+  return (
+    <Box width="256px" pr="4" borderRight="2px solid" ml="-2" borderRightColor="gray.200" height="calc(100vh - 3.5rem - 4rem)" overflowY="auto" overflowX="auto">
+      <Heading as="h3" fontSize="xl" color="cyan.400" ml="2" mb="8">Daftar Copypasta</Heading>
+      <List listStyleType="none">
+      {Templates.map(item => (
+        <ListItem
+          key={item.name}
+        >
+          <Button
+            justifyContent="start"
+            bg="transparent"
+            color="gray.600"
+            width="100%"
+            p="2"
+            borderRadius="4px"
+            transition="background 0.2s"
+            cursor="pointer"
+            _hover={{
+              bg: 'cyan.100'
+            }}
+            onClick={() => onSelect(item)}
+          >
+            <Text fontSize="lg">{item.name}</Text>
+          </Button>
+        </ListItem>
+      ))}
+      </List>
+    </Box>
+  )
+}
+
+Sidebar.defaultProps = {
+  onSelect: () => {
+    // nothing
+  }
+}
+
+export default Sidebar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,78 @@
-import React from 'react'
-import { Flex } from '@chakra-ui/react'
+import React, { useState, useEffect, ChangeEvent } from 'react'
+import { Box, Flex, Heading, Text, Input, FormControl, FormLabel, Container } from '@chakra-ui/react'
 
 import Layout from '../layout/Default'
+
+import Template from '../types/Template'
 
 import Sidebar from '../components/Sidebar'
 
 const Index = (): React.ReactElement => {
+  const [template, setTemplate] = useState<Template>()
+  const [inputValues, setInputValues] = useState<Record<string, string>>({})
+
+  useEffect(() => {
+    if (template) {
+      setInputValues({ ...template.fields })
+    }
+  }, [template])
+
+  const getInputFields = (): [string, string][] => {
+    return Object.entries(template?.fields)
+  }
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>): void => {
+    const changed = { ...inputValues }
+    changed[e.target.name] = e.target.value
+    setInputValues(changed)
+  }
+
   return (
     <Layout>
       <Flex>
-        <Sidebar />
+        <Sidebar onSelect={setTemplate}/>
+        <Box pl="4">
+          <Heading as="h1" color="gray.600">{template?.name ?? 'Silakan pilih copypasta terlebih dahulu'}</Heading>
+          {template && (
+            <>
+              <Box mt="8">
+                <Text fontSize="lg" fontWeight="bold" color="gray.500" mb="4">INPUT</Text>
+                {getInputFields().map(([fieldName, fieldLabel]) => (
+                  <FormControl key={fieldName}>
+                    <FormLabel color="gray.600">{fieldLabel}</FormLabel>
+                    <Input
+                      type="text"
+                      borderColor="gray.400"
+                      borderRadius="4px"
+                      name={fieldName}
+                      onChange={handleInput}
+                      value={inputValues[fieldName]}
+                    />
+                  </FormControl>
+                ))}
+              </Box>
+              <Box mt="8">
+              <Text fontSize="lg" fontWeight="bold" color="gray.500" mb="4">OUTPUT</Text>
+              <Container p="0">
+              {template.template(inputValues).split(/\n/g).map(line => (
+                line.length > 0
+                  ? (
+                  <>
+                    <Text as="p">
+                      {line}
+                    </Text>
+                    <br />
+                  </>
+                    )
+                  : (
+                  <></>
+                    )
+              ))}
+              </Container>
+            </Box>
+            </>
+          )}
+        </Box>
       </Flex>
     </Layout>
   )
