dd0398e435adff7d2622d07d333ca34c1757e776
Dhika Rizky <deka_r@outlook.com>
2021-01-16 09:12:16 +0700

    Added some image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 
 const App = ({ Component, pageProps }: AppProps): React.ReactElement => {
   return (
     <ChakraProvider>
+      <Head>
+        <title>pasta - Copypasta Untuk Semua</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
