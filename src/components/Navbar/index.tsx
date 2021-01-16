ba5304321b8fe2fac6e25ffac7b4c525363040a4
Dhika Rizky <deka_r@outlook.com>
2021-01-16 07:15:00 +0700

    Updated navbar styling

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,8 +3,8 @@ import { Flex, Box, Heading } from '@chakra-ui/react'
 
 const Navbar = (): React.ReactElement => {
   return (
-    <Box px="4" py="2" borderTopWidth={4} borderTopColor="cyan.400" borderTopStyle="solid" as="nav" bg="gray.50" position="fixed" zIndex="sticky" w="100vw" top="0">
-      <Flex maxW="6xl" marginX="auto">
+    <Box px="4" py="2" h="16" borderTopWidth={4} borderTopColor="cyan.400" borderTopStyle="solid" as="nav" bg="white" boxShadow="sm" position="fixed" zIndex="sticky" w="100vw" top="0">
+      <Flex maxW="6xl" marginX="auto" h="100%" alignItems="center">
         <Box>
           <Heading size="lg" color="gray.600">pasta</Heading>
         </Box>
