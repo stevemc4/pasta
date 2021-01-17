32965926a1beac9fa3428eb4ffc8b860c9b87a4e
Dhika Rizky <deka_r@outlook.com>
2021-01-17 20:30:49 +0700

    Added web manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,16 @@ const App = ({ Component, pageProps }: AppProps): React.ReactElement => {
       <Head>
         <title>pasta - Copypasta Untuk Semua</title>
         <meta name="description" content="pasta adalah sebuah aplikasi web untuk membuat copypasta." />
+        <link rel="manifest" href="manifest.json" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="pasta" />
+        <meta name="apple-mobile-web-app-title" content="pasta" />
+        <meta name="theme-color" content="#0BC5EA" />
+        <meta name="msapplication-navbutton-color" content="#0BC5EA" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="msapplication-starturl" content="/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
