4647d1ef0d0cfb5cb0a4d0d7e7a58b3fd1ba923a
Dhika Rizky <deka_r@outlook.com>
2021-11-08 21:44:58 +0700

    Added Miyazono Kaori's last letter copypasta

---
+++ src/templates/suratKaori.ts
@@ -0,0 +1,132 @@
+import Template from '../types/Template'
+
+const SuratKaori: Template = {
+  name: 'Surat Kaori',
+  template: () => `
+  Teruntuk Arima Kousei-sama
+
+  Rasanya aneh menulis surat kepada seseorang yang baru saja bersamaku
+
+  Kau itu orang yang keterlaluan
+  
+  Dungu, lemot, bodoh
+
+  Pertama kali ku melihatmu adalah ketika ku berusia 5 tahun
+  Pada saat itu di sebuah pertunjukkan kelas piano
+  Seorang anak yang tampil canggung itu berada di depan piano yang terlalu besar baginya dan kesulitan mengatur tempat duduknya sehinggi mengundang tawa penonton
+  Dan ketika ia melantunan nada pertamanya, langsung membuatku terpukau.
+
+  Nadanya seolah memiliki 24 corak warna dan melodinya menari-nari dengan penuh warna
+  
+  Aku sangat terkejut saat anak perempuan yang duduk disampingku tiba-tiba menangis kencang
+
+  Meski begitu kau malah berhenti bermain piano
+
+  Padahal kau sudah mempengaruhi hidup seseorang, kau sungguh keterlaluan
+  
+  Jahat, lemot, bodoh
+
+  Saat kutahu kita berada di SMP yang sama jiwaku rasanya melayang
+  Bagaimana ya caranya agar aku bisa menyapamu
+  Apa ku ajak beli sandwich di kantin sekolah aja ya
+  Namun, pada akhirnya aku hanya bisa memandangimu saja
+  Soalnya, semua orang terlihat begitu dekat denganmu
+  Tak ada ruang sedikitpun untukku masuk
+  
+  Saat aku kecil aku pernah dioperasi
+  Lalu sering dirawat di rumah sakit
+  Setelah aku jatuh pingsan saat kelas 1 SMP
+  Aku jadi lebih sering keluar masuk rumah sakit
+  Dan aku pun dirawat jadi semakin lebih lama
+  Aku jadi tidak bisa sekolah dengan semestinya
+  Aku tahu kalau kondisi tubuhku tidak begitu baik
+
+  Suatu malam
+  Aku melihat Ibu dan Ayahku menangis di ruang tunggu rumah sakit
+  Aku sadar kalau waktuku sudah tak banyak lagi
+  
+  Saat itulah...
+  Aku... mulai berlari
+  
+  Aku mulai melakukan apapun yang ku mau
+  Supaya aku tak membawa penyesalan ke akhirat nanti
+  Aku tak takut lagi untuk memakai lensa kontak
+  Memakan banyak kue yang sebelumnya tak bisa ku lakukan karena khawatir dengan berat badanku
+  Partitur musik yang selama ini selalu mengaturku
+  Sekarang aku memainkannya dengan caraku sendiri
+  
+  Dan...
+  Aku mengucapkan satu kebohongan
+  Bahwa Miyazono Kaori menyukai Watari Ryota-kun adalah kebohonganku
+  Kebohongan itu
+  Membawakan depanku
+
+  Arima Kousei-kun
+  Kebohongan itu membawamu padaku
+  
+  Sampaikan permintaan maafku pada Watari-kun
+  Aah, tapi, sepertinya kalau Watari-kun, pasti sudah bisa melupakanku
+  Sebagai teman dia menyenangkan
+  Tapi sepertinya aku lebih menyukai orang yang setia
+  
+  Lalu, sampaikan juga permintaan maafku pada Tsubaki-chan
+  Aku hanyalah seseorang yang hanya numpang lewat dan akan menghilang
+  Karena aku tak ingin meninggalkan kesan buruk yang aneh, jadi aku tak bisa meminta padanya
+  Atau bisa dibilang, meski aku meminta berkenalan dengan Arima-kun padanya secara langsung, ku rasa Tsubaki-chan tak akan memberikan balasan yang baik
+  Lagipula Tsubaki-chan sangat menyukaimu
+  Semua orang tahu itu
+  Yang tidak mengetahuinya hanya kamu dan Tsubaki-chan saja
+  Kebohongan licik yang membawamu padaku
+  Tak pernah kubayangkan sebelumnya
+
+  Kau jauh lebih suram dan murung dari yang kuduga
+  Kau juga keras kepala, tak kenal lelah dan tukang intip
+
+  Suaramu jauh lebih pelan dari yang kuduga
+  Dan kau jauh lebih jantan dari yang kuduga
+  Namun sesuai dugaanku kau pria yang baik
+  
+  Saat kita melompat ke sungai
+  Airnya sangat sejuk dan menyegarkan ya?
+  Bulan yang mengintip ke ruang musik
+  Seperti roti manju yang enak
+  Saat kita balapan dengan kereta
+  Aku sangat yakin kita bisa menang
+  Saat kita berdua bernyanyi Twinkle, Twinkle Little Star rasanya menyenangkan sekali ya?
+  Sekolah di malam hari ternyata memang ada ya
+  Salju yang turun terlihat seperti kelopak bunga sakura ya?
+
+  Aku adalah musisi namun terpengaruh oleh segala sesuatu dari luar panggung
+  Aku ini orangnya aneh ya?
+  Aneh ya pemandangan yang tak terlupakan adalah hal-hal biasa seperti ini
+
+  Bagaimana denganmu
+  Apa aku berhasil masuk ke dalam hati seseorang?
+  Apa aku berhasil masuk ke dalam hatimu?
+  Meski hanya sedikit, apa kau akan mengingatku?
+
+  Jangan direset ya?
+
+  Jangan melupakanku ya? Janji ya?
+
+  Ternyata benar, aku senang bertemu denganmu.
+  Bisa tersampaikan ngga ya? Kuharap tersampaikan
+  
+  Arima Kousei-kun
+  Aku mencintaimu
+  Mencintaimu
+  Mencintaimu
+  
+  Maaf aku tak menghabiskan Canèlenya ya
+  Maaf aku karena sering memukulmu ya
+  Maaf aku sudah egois
+  Maafkan aku atas segalanya ya
+
+  Terima kasih
+  
+  P.S.: Aku menaruh benda paling berhargaku di dalam suratnya,
+  jika kau tak menginginkannya, robek dan buang saja
+  `
+}
+
+export default SuratKaori
